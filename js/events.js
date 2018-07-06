edd7cb93fba787883d9aab633cc90bf04eb5633f
christleyjordan@gmail.com Christley <christleyjordan@gmail.com>
2018-07-06 22:07:29 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,13 @@
 //define functions here
+function getIt() {
+  $("p").on("click", () => alert("Hey!"));
+}
 
+function frameIt() {
+  $('img').on('load', () => {
+    this.addClass
+  })
+}
 $(document).ready(function(){
 
 // call functions here
